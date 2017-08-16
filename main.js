47c9c3f3abd19b38aece6134b366b7689a0c7b79
ns23 <niteshsawant023@gmail.com>
2017-08-17 00:38:18 +0530

    function get size of videos before download beta stage

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ let yt = require('./download_video');
 let downloadQueue = new Array();
 let i = 0;
 
+
 let x = new Utube();
-x.parsePlaylist('PL6gx4Cwl9DGDi9F_slcQK7knjtO8TUvUs').then(function(res) {
+x.getVideoSize();
+
+/* x.parsePlaylist('PL6gx4Cwl9DGDi9F_slcQK7knjtO8TUvUs').then(function(res) {
     console.log(res); //list of videos in youtue playlist
 }).catch(function(error) {
     console.log(error);
@@ -14,16 +17,17 @@ x.parsePlaylist('PL6gx4Cwl9DGDi9F_slcQK7knjtO8TUvUs').then(function(res) {
 
 
 
-/* function cb(params = null) {
+function cb(params = null) {
     console.log('Video id is');
     console.log(params);
 }
 
 x.saveVideoToJson('QjxScn7cKo8').then(function(res) {
     let info = extractInfo(res);
+    console.log(info);
     info.videoCount = i;
     i++;
-    downloadQueue.push(new yt(info, cb));
+    // downloadQueue.push(new yt(info, cb));
 
 
 }); */
